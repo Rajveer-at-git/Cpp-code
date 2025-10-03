16959fb7ebdd14e6e4cbd9f43b54ca37968d68c0
Rajveer <levelingup1923@gmail.com>
2025-10-03 17:03:35 +0530

    20th file

---
+++ 20.cpp
@@ -0,0 +1,3 @@
+// Imp question for exam 
+// Differentiate between non static and static data members of the class with example 
+// Differentiate between non static member function and static member function of the class with example 

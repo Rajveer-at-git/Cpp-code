589cb769f21bf16092afd63b3de4dfa1f91cabfc
Rajveer <levelingup1923@gmail.com>
2025-10-05 08:52:14 +0530

    22nd File

---
+++ 22.cpp
@@ -0,0 +1,40 @@
+// Friend function
+# include<iostream>
+using namespace std;
+class ABC;
+class XYZ
+{
+    int x;
+    public:
+    void setvalue(int i)
+    {
+        x = i;
+    }
+    friend void max(XYZ,ABC);
+};
+class ABC
+{
+    int a;
+    public:
+    void setvalue(int j)
+    {
+        a = j;
+    }
+    friend void max(XYZ,ABC);
+};
+void max(XYZ m, ABC n)
+{
+    if(m.x > n.a)
+    cout << m.x << endl;
+    else
+    cout << n.a << endl;
+}
+
+int main()
+{
+    ABC abc;
+    abc.setvalue(10);
+    XYZ xyz;
+    xyz.setvalue(20);
+    max(xyz,abc);
+}

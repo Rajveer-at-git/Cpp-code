d2ba09c5166a07b39c0d9553a01b2f35a3a1dfd2
Rajveer <levelingup1923@gmail.com>
2026-01-01 20:12:37 +0530

    Polymorphism and virtual Fn

---
+++ 45 polymorphism and virtual Fn.cpp
@@ -0,0 +1,20 @@
+// Function Overloading
+#include <iostream>
+using namespace std;
+
+class Print {
+public:
+    void show(int x) {
+        cout << "Integer: " << x << endl;
+    }
+    void show(string s) {
+        cout << "String: " << s << endl;
+    }
+};
+
+int main() {
+    Print p;
+    p.show(100);
+    p.show("Hello");
+    return 0;
+}
